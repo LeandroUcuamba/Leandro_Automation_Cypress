0b247192a33d9b4736678bb1bc53401427f62860
Leandro Ucuamba <leandrodossantos.lc5@gmail.com>
2025-02-16 20:43:43 +0100

    add - reports

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,25 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    reporter: 'cypress-multi-reporters',
+    reporterOptions: {
+      reporterEnabled: 'cypress-mochawesome-reporter, mocha-junit-reporter',
+      mochaJunitReporterReporterOptions: {
+        mochaFile: 'cypress/reports/junit/results-[hash].xml',
+        toConsole: true,
+      },
+      cypressMochawesomeReporterReporterOptions: {
+        charts: true,
+        reportPageTitle: 'Relatório de Teste',
+        embeddedScreenshots: true,
+        inlineAssets: true,
+        saveAllAttempts: false,
+      },
     },
-  },
+    chromeWebSecurity: false,
+    e2e: {
+       setupNodeEvents(on, config) {
+          require('cypress-mochawesome-reporter/plugin')(on);
+       },
+    },
 });
