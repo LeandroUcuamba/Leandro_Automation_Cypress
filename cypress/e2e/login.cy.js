7272f8d43bef3f04cbb4f9bca5b7631388377fd1
Leandro Ucuamba <leandrodossantos.lc5@gmail.com>
2025-02-16 12:57:26 +0100

    add - login test

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,5 +1,33 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Login', () => {
+  it('Realizar login com sucesso', () => {
+     // Arrange
+     cy.visit('https://www.saucedemo.com/')
+
+     // Act
+     cy.get('[data-test="username"]').type('standard_user');
+     cy.get('[data-test="password"]').type('secret_sauce');
+     cy.get('[data-test="login-button"]').click();
+
+     // Assert
+     cy.url().should('eq', 'https://www.saucedemo.com/inventory.html');
   })
+
+  it('Realizar login informando crendenciais incorrectas', () => {
+    // Arrange
+    cy.visit('https://www.saucedemo.com/')
+
+    // Act
+    cy.get('[data-test="username"]').type('invalid_user');
+    cy.get('[data-test="password"]').type('test00000');
+    cy.get('[data-test="login-button"]').click();
+
+    cy.get('[data-test="error"]')
+      .should(
+        "contain.text",
+        "Epic sadface: Username and password do not match any user in this service"
+      )
+
+    // Assert
+    cy.url().should('eq', 'https://www.saucedemo.com/');
+ })
 })
