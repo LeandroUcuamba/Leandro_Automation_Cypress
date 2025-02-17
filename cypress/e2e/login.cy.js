b0c7c01363a64e99a16b6ef95710ed115791383c
Leandro Ucuamba <leandrodossantos.lc5@gmail.com>
2025-02-17 20:01:43 +0100

    refatorando o codigo - 17/02

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -4,30 +4,43 @@ require('cypress-xpath');
 
 describe('Login', () => {
 
+  beforeEach(() => {
+    cy.visit('http://www.juliodelima.com.br/taskit/');
+  });
+
+  const nomeAleatorio = Math.random().toString(36).substring(2, 9);
+  const passwordAleatoria = Math.random().toString(36).substring(7, 14);
+
   it('Sign-up for an account successfully', () => {
-     // Arrange
-     cy.visit('http://www.juliodelima.com.br/taskit/');
 
-     const numeroAleatorio = Math.random().toString(36).substring(2, 9);
-
-     // Act
      cy.get('#signup').click();
      cy.get('#name-sign-up').type('Leandro Ucuamba');
-     cy.get('#login-sign-up').type(`User_${numeroAleatorio}`);
-     cy.get('#password-sign-up').type('testArrop');
+     cy.get('#login-sign-up').type(`${nomeAleatorio}`);
+     cy.get('#password-sign-up').type(`${passwordAleatoria}`);
      cy.get('#btn-submit-sign-up').click();
 
-     // Assert
      cy.get('body > div.container > div > div > p:nth-child(2)')
         .should(
             "contain.text",
             "You are now registered and can add tasks you want to do in the distant future."
         )
+  })
+  it('Login test with wrong "user" in the credentials.', () => {
 
-     cy.xpath('/html/body/nav/div/div/ul[1]/li[3]/a').click();
-     cy.xpath('/html/body/nav/div/div/ul[1]/li/a').click();
-     cy.get('#login-sign-in').type(`User_${numeroAleatorio}`);
-     cy.get('#password-sign-in').type('testArrop');
-     cy.get('#btn-submit-sign-in').click();
+    cy.get('nav a').contains('Sign in').click();
+    cy.get('#login-sign-in').type("wrong_user");
+    cy.get('#password-sign-in').type("testArrop_");
+    cy.get('#btn-submit-sign-in').click();
+    cy.get('#toast-container').should('be.visible');
+    cy.get('#toast-container').should('contain.text', 'Maybe you brain dropped the password or login in some place!');
+  })
+  it('Login test with wrong "password" in the credentials.', () => {
+    
+    cy.get('nav a').contains('Sign in').click();
+    cy.get('#login-sign-in').type("User_");
+    cy.get('#password-sign-in').type("wrong_password");
+    cy.get('#btn-submit-sign-in').click();
+    cy.get('#toast-container').should('be.visible');
+    cy.get('#toast-container').should('contain.text', 'Maybe you brain dropped the password or login in some place!');
   })
 })
